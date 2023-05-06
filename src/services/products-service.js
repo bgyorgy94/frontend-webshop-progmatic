4f241b36eaa1d5f0cba17cd421bee65b95922508
Reisch <reischemese@gmail.com>
2023-05-06 11:31:22 +0200

    products-service fetch objektum

---
+++ src/services/products-service.js
@@ -0,0 +1,63 @@
+const API_URL= "https://csapat-10-default-rtdb.europe-west1.firebasedatabase.app/termekek"
+
+
+function createProduct(product) {
+    return(
+        fetch(`${API_URL}.json`,{
+            method:'POST',
+            headers:{
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(product)
+        })
+        .then(res=> res.json())
+        )   
+    }
+
+    function getAllProducts() {
+        return(
+            fetch(`${API_URL}.json`)
+            .then(res => res.json())
+        )
+    }
+
+    function getProduct(id){
+        return(
+            fetch(`${API_URL}/${id}.json`)
+            .then(res => res.json())
+        )
+    }
+
+    function updateProduct(id,product){
+        return(
+            fetch(`${API_URL}/${id}.json`,{
+                method: 'PUT',
+                headers:{
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(product)
+            })
+            .then(res => res.json())
+        )
+    }
+
+    function deleteProduct(id,successCallback) {
+        return(
+            fetch(`${API_URL}/${id}.json`, {
+                method: 'Delete'
+            })
+            .then(res => res.json())
+            .then(json => successCallback(json))
+        )
+    }
+    
+
+
+
+export default{
+    createProduct: createProduct,
+    getAllProducts: getAllProducts,
+    getProduct: getProduct,
+    updateProduct: updateProduct,
+    deleteProduct: deleteProduct
+}

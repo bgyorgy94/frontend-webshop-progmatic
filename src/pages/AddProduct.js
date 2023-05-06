5a7392a2f4223ce7c1039f461d55e183c7e37e30
TamasPeterProgmat <geraldogeraldes88@gmail.com>
2023-05-06 15:46:38 +0200

    addProductPage

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,5 +1,36 @@
+import { useState } from "react";
+
 export default function AddProduct() {
+
+const [title, setTitle] = useState ("")
+const [price, setPrice] = useState ("")
+
+function handleTitleChange(e){
+    let value = e.target.value;
+    setTitle(value)  
+}
+function handlePriceChange(e){
+    let value = e.target.value;
+    setPrice(value)  
+}
+
+function handlerSubmit(e){
+    e.preventDefault()
+    console.log (title, price)
+}
     return(
-        <div>Add product</div>
+        <div>Add product</div>,
+        <form onSubmit={handlerSubmit}> 
+            <p>
+                <input type = "text" onChange={handleTitleChange} value = {title}/>
+            </p>
+            <p>
+                <input type = "number" onChange={handlePriceChange} value = {price}/>
+            </p>
+            <p>
+                <button >Submit</button>
+            </p>
+        </form>
+              
     )
 }

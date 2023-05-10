e1f0332b41e64f60b1fe743c7f09e8027aa5f054
dokahunter <tarmilan@gmail.com>
2023-05-10 19:25:53 +0200

    Add new product to database

--- src/pages/AddProduct.js
+++ src/pages/AddProduct.js
@@ -1,36 +1,49 @@
 import { useState } from "react";
+import productsService from "../services/products-service";
 
 export default function AddProduct() {
 
-const [title, setTitle] = useState ("")
-const [price, setPrice] = useState ("")
+    const [title, setTitle] = useState("")
+    const [price, setPrice] = useState("")
 
-function handleTitleChange(e){
-    let value = e.target.value;
-    setTitle(value)  
-}
-function handlePriceChange(e){
-    let value = e.target.value;
-    setPrice(value)  
-}
-
-function handlerSubmit(e){
-    e.preventDefault()
-    console.log (title, price)
-}
-    return(
-        <div>Add product</div>,
-        <form onSubmit={handlerSubmit}> 
+    return (
+        <>
+        <div>Új termék hozzáadása:</div>
+        <form onSubmit={handlerSubmit}>
             <p>
-                <input type = "text" onChange={handleTitleChange} value = {title}/>
+                Termék neve:
+                <input type="text" onChange={handleTitleChange} value={title} />
             </p>
             <p>
-                <input type = "number" onChange={handlePriceChange} value = {price}/>
+                Termék ára:
+                <input type="number" onChange={handlePriceChange} value={price} />
             </p>
             <p>
-                <button >Submit</button>
+                <button>Termék hozzáadása</button>
             </p>
         </form>
-              
+        </>
+
     )
+
+    function handleTitleChange(e) {
+        let value = e.target.value;
+        setTitle(value)
+    }
+    function handlePriceChange(e) {
+        let value = e.target.value;
+        setPrice(value)
+    }
+
+    function handlerSubmit(e) {
+        e.preventDefault()
+        console.log(title, price)
+        productsService.createProduct({
+            name: title,
+            price: price
+        })
+        setTitle("");
+        setPrice("");
+    }
+
 }
--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,10 +1,11 @@
-import { Outlet } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import AdminDashboard from "./AdminDashboard";
 
 export default function Admin() {
     return(
         <div>
             Admin
+            <Link to="./termek-felvitel">Új termék</Link>
             <Outlet />
         </div>
     )

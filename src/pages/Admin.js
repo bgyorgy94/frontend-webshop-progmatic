e709e8bb5149c2572455e713135fed962977dffc
dokahunter <tarmilan@gmail.com>
2023-05-24 00:01:44 +0200

    Admin Nav w/ tabs

--- src/pages/Admin.js
+++ src/pages/Admin.js
@@ -1,4 +1,4 @@
-import { Link, Navigate, Outlet } from "react-router-dom";
+import { Link, NavLink, Navigate, Outlet } from "react-router-dom";
 import AdminDashboard from "./AdminDashboard";
 import { useContext } from "react";
 import { UserContext } from "../contexts/userContext";
@@ -6,13 +6,31 @@ import { UserContext } from "../contexts/userContext";
 export default function Admin() {
     const [user] = useContext(UserContext)
 
-    if(user === null || !user.isAdmin) {
-        return <Navigate to={"/belepes"}/>
+    if (user === null || !user.isAdmin) {
+        return <Navigate to={"/belepes"} />
     }
-    return(
+    return (
         <div>
-            Admin
-            <Link to="./termek-felvitel">Új termék</Link>
+            <ul className="nav nav-tabs">
+                <li class="nav-item">
+                    <NavLink className="nav-link" to="/admin/termekek">Termékek</NavLink>
+                </li>
+                <li>
+                    <NavLink className="nav-link" to="/admin/termek-felvitel">Új termék</NavLink>
+                </li>
+                <li>
+                    <NavLink className="nav-link" to="/admin/kategoriak">Kategóriák</NavLink>
+                </li>
+                <li>
+                    <NavLink className="nav-link" to="/admin/uj-kategoria">Új kategória</NavLink>
+                </li>
+                <li>
+                    <NavLink className="nav-link" to="/admin/vasarlok">Vásárlók</NavLink>
+                </li>
+                <li>
+                    <NavLink className="nav-link" to="/admin/megrendelesek">Megrendelések</NavLink>
+                </li>
+            </ul>
             <Outlet />
         </div>
     )

aaef72e55e8fb794e6a0e149403188c3efcd3568
bgyorgy94 <bgyorgy94@gmail.com>
2023-05-24 19:01:43 +0200

    modified navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,6 +5,8 @@ import CartSum from "../CartSum/CartSum";
 import DisplayUser from "../DisplayUser/DisplayUser"
 import { Navbar as BootstrapNavbar, Container, Nav } from "react-bootstrap";
 import "../Navbar/navbar.scss"
+import { Cart } from 'react-bootstrap-icons';
+import Button from 'react-bootstrap/Button';
 
 
 export default function Navbar() {
@@ -13,12 +15,16 @@ export default function Navbar() {
     return (
         <BootstrapNavbar sticky="top" className="navbar">
             <Container>
+                    <div className="d-inline-flex navbar-left">
                     <Nav.Link as={NavLink} to="/">Home</Nav.Link>
                     <Nav.Link as={NavLink} to="/termekek">Termékek</Nav.Link>
                     {user !== null && user.isAdmin ? <Nav.Link as={NavLink} to="/admin/termekek">Admin</Nav.Link> : ""}
-                    <Nav.Link as={NavLink} to="/kosar">Kosár ({<CartSum />})</Nav.Link>
+                    </div>
+                    <div className="d-inline-flex navbar-right">
+                    <Nav.Link as={NavLink} to="/kosar"><Button type="button" class="btn btn-outline-primary"><Cart />{<CartSum />}</Button></Nav.Link>
                     {user ? <DisplayUser /> : ""}
                     {user ? "" : <Nav.Link as={NavLink} to="/belepes">Belépés</Nav.Link>}
+                    </div>
             </Container>
         </BootstrapNavbar>
     )

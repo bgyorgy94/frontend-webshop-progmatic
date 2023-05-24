cfeab6628436849d6b306e027e76f7a64828f9c1
ImLumi <mikesimon150@gmail.com>
2023-05-24 20:25:39 +0200

    fix(adminProduct): actions column width.

--- src/components/AdminProductTable/AdminProductTable.js
+++ src/components/AdminProductTable/AdminProductTable.js
@@ -4,71 +4,85 @@ import productsService from "../../services/products-service";
 import sortProducts from "../../services/sortProducts";
 import pagerService from "../../services/pager-service";
 import Pager from "../Pager/Pager";
-import Toast from "../Toast/Toast";
+import ProductNameSorter from "../ProductNameSorter/ProductNameSorter";
+import ProductPriceSorter from "../ProductPriceSorter/ProductPriceSorter";
 
-export default function AdminProductTable({children}) {
-    const [products, setProducts] = useState([]);
-    const [usp] = useSearchParams();
-    const pagerData = pagerService(usp)
+export default function AdminProductTable() {
+  const [products, setProducts] = useState([]);
+  const [usp] = useSearchParams();
+  const pagerData = pagerService(usp)
 
-    useEffect(() => {
-        productsService.getAllProducts()
-        .then(json => {
-            const originalProducts = (Object.values(json))
-            const title = usp.get("title") ;
-            const minimumPrice = usp.get("minimumPrice") || 0;
-            const maximumPrice = usp.get("maximumPrice") || Number.MAX_SAFE_INTEGER;
-            const category = usp.get("category");
-            let titleFiltered;
-            let categoryFiltered;
+  useEffect(() => {
+    productsService.getAllProducts()
+      .then(json => {
+        const originalProducts = (Object.values(json))
+        const title = usp.get("title");
+        const minimumPrice = usp.get("minimumPrice") || 0;
+        const maximumPrice = usp.get("maximumPrice") || Number.MAX_SAFE_INTEGER;
+        const category = usp.get("category");
+        let titleFiltered;
+        let categoryFiltered;
 
-            if (title !== null && title !== "") {
-                titleFiltered = originalProducts.filter((product) => (
-                    product.name.toLowerCase().includes(title)))
-            } else titleFiltered = originalProducts;
+        if (title !== null && title !== "") {
+          titleFiltered = originalProducts.filter((product) => (
+            product.name.toLowerCase().includes(title)))
+        } else titleFiltered = originalProducts;
 
-            if (category === "uncategorized") {
-                categoryFiltered = titleFiltered.filter((product) => (
-                    product.categoryId === "" || !product.categoryId
-                ))
-            } else if (category !== null) {
-                categoryFiltered = titleFiltered.filter((product) => (
-                    product.categoryId === category
-                ))
-            } else categoryFiltered = titleFiltered;
+        if (category === "uncategorized") {
+          categoryFiltered = titleFiltered.filter((product) => (
+            product.categoryId === "" || !product.categoryId
+          ))
+        } else if (category !== null) {
+          categoryFiltered = titleFiltered.filter((product) => (
+            product.categoryId === category
+          ))
+        } else categoryFiltered = titleFiltered;
 
-            return (
-                categoryFiltered.filter((product) => (
-                    Number(product.price) >= Number(minimumPrice) && Number(product.price) <= Number(maximumPrice)
-                ))
-            )
-        })
-        .then(json => {
-            const sortBy = usp.get("sortBy");
-            const direction = usp.get("direction");
-            setProducts(sortProducts(json, sortBy, direction))})
-    }, [usp])
+        return (
+          categoryFiltered.filter((product) => (
+            Number(product.price) >= Number(minimumPrice) && Number(product.price) <= Number(maximumPrice)
+          ))
+        )
+      })
+      .then(json => {
+        const sortBy = usp.get("sortBy");
+        const direction = usp.get("direction");
+        setProducts(sortProducts(json, sortBy, direction))
+      })
+  }, [usp])
 
-    return (
-        <>
-            <table className="table table-hover">
-                    <thead>
-                        <tr>
-                            {children}
-                        </tr>
-                    </thead>
-                <tbody>
-                    {products.slice(pagerData.startIdx, pagerData.endIdx).map((product, idx) => {
-                        return (
-                            <tr key={idx}>
-                                <td className="align-middle">{product.name}</td>
-                                <td className="text-end w-auto align-middle">{product.price} Ft</td>
-                                <td className="w-auto"><Link className="btn btn-primary mx-1" to={`/admin/termekek/${product.id}/modositas`}>Módosítás</Link>
-                                <Link className="btn btn-danger mx-1" to={`/admin/termekek/${product.id}/torles`}>Törlés</Link></td>
-                            </tr>)})}
-                </tbody>
-            </table>
-            <Pager allProducts={products.length} itemsPerPage={pagerData.itemsPerPage} />
-        </>
-    )
+  return (
+    <>
+      <table className="table table-hover">
+        <thead>
+        <tr>
+          <th className="text-nowrap">
+            <ProductNameSorter name="Termék neve"/>
+          </th>
+          <th className="text-end text-nowrap">
+            <ProductPriceSorter/>
+          </th>
+          <th/>
+        </tr>
+        </thead>
+        <tbody>
+        {products.slice(pagerData.startIdx, pagerData.endIdx).map((product, idx) => {
+          return (
+            <tr key={idx}>
+              <td className="align-middle">{product.name}</td>
+              <td className="text-end w-auto align-middle">{product.price} Ft</td>
+              <td className="text-end w-1">
+                <div className="btn-group">
+                  <Link className="btn btn-outline-primary"
+                        to={`/admin/termekek/${product.id}/modositas`}>Módosítás</Link>
+                  <Link className="btn btn-danger" to={`/admin/termekek/${product.id}/torles`}>Törlés</Link>
+                </div>
+              </td>
+            </tr>)
+        })}
+        </tbody>
+      </table>
+      <Pager allProducts={products.length} itemsPerPage={pagerData.itemsPerPage}/>
+    </>
+  )
 }
--- src/pages/AdminProducts.js
+++ src/pages/AdminProducts.js
@@ -1,21 +1,11 @@
 import AdminProductTable from "../components/AdminProductTable/AdminProductTable";
 import Filter from "../components/Filter/Filter";
-import ProductNameSorter from "../components/ProductNameSorter/ProductNameSorter";
-import ProductPriceSorter from "../components/ProductPriceSorter/ProductPriceSorter";
 
 export default function AdminProducts() {
     return (
         <div className="container">Admin Products
             <Filter />
-            <AdminProductTable >
-                <th>
-                    <ProductNameSorter name="Termék neve" />
-                </th>
-                <th className="text-end">
-                    <ProductPriceSorter />
-                </th>
-                <th />
-            </AdminProductTable >
+            <AdminProductTable />
         </div>
     )
 }

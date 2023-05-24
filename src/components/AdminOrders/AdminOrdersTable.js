9b5c2a26340be3b16f77eed3ef9634a9652b5a8e
ImLumi <mikesimon150@gmail.com>
2023-05-24 19:52:08 +0200

    refactor(AdminOrder): AdminOrders.js and AdminOrdersTable.js

--- src/components/AdminOrders/AdminOrdersTable.js
+++ src/components/AdminOrders/AdminOrdersTable.js
@@ -1,125 +1,67 @@
-import { useEffect, useState } from "react"
-import orderService from "../../services/order-service";
-import userService from "../../services/user-service";
-import productsService from "../../services/products-service";
-import sortProducts from "../../services/sortProducts";
-import { useSearchParams } from "react-router-dom";
-import Pager from "../Pager/Pager";
-import pagerService from "../../services/pager-service";
-import Table from 'react-bootstrap/Table';
-import "../UserOrderList/userOrderList.css"
+import ProductNameSorter from "../ProductNameSorter/ProductNameSorter";
+import Table from "react-bootstrap/Table";
 
-export default function AdminOrdersTable({children}) {
-
-    const [orderDatas,setOrderDatas] = useState([]);
-    const [userDatas,setUserDatas] = useState([]);
-    const [usp] = useSearchParams();
-    const pagerData = pagerService(usp);
-    const name = usp.get("title")
-    const [sortedOrders,setSortedOrders] = useState([]);
-
-    let sum=0;
-    useEffect( () => {
-        orderService.getOrders()
-        .then(json=> {
-            setOrderDatas(Object.values(json))
-        })
-
-        userService.getUserDatas()
-        .then(json => {
-            let originalUserDatas = Object.values(json)
-            let namesFiltered
-
-            if (name !== null) {
-                namesFiltered = originalUserDatas.filter((user) => (
-                    user.firstName.toLowerCase().includes(name) || user.lastName.toLowerCase().includes(name)
-                ))
-            } else namesFiltered = originalUserDatas
-
-            setUserDatas(namesFiltered)
-            
-            const direction = usp.get("direction");
-        orderService.getOrders()
-        .then(json => {
-            direction == null ? 
-            setSortedOrders(Object.values(json)) 
-            : 
-            setSortedOrders( direction === "asc" ? 
-            orderDatas.sort((a, b) => (userDatas.filter(user=> user.id == a.uid)[0].lastName> userDatas.filter(user=> user.id == b.uid)[0].lastName) ? 1 : -1)
-            :
-            orderDatas.sort((a, b) => (userDatas.filter(user=> user.id == a.uid)[0].lastName> userDatas.filter(user=> user.id == b.uid)[0].lastName) ? -1 : 1)
-            )  
-        })
-        })
-    },[usp])
-    console.log(usp.get("direction"))
-    
-
-    
-    return(
-        <>
-        <Table striped bordered hover>
-            <thead>
-                <tr>
-                    <th>Vásárló neve</th>
-                    <th>Megrendelés ID</th>
-                    <th>Termékek</th>
-                    <th>Ár</th>
-                    <th>Mennyiség</th>
-                    <th>Megrendelés összege</th>
-                </tr>
-            </thead>
-            <tbody>
-            {sortedOrders.filter((order) => (
-                userDatas.map((user) => {return user.id}).includes(order.uid)
-            )).slice(pagerData.startIdx, pagerData.endIdx).map( (order,idx) => {
-                sum =0;
-                return(
-                    <tr key={idx}>
-                         <td> {
-                            userDatas.filter( user => user.id == order.uid)
-                            .map(user => user.lastName + " " + user.firstName)
-                            }</td>  
-                        <td> {order.id}</td> 
-                        <td>
-                            <ul>
-                                {
-                                Object.entries(order.termekek).map((product,idx) =>{
-                                    return(
-                                        <li key={idx}>{product[1].name}</li>
-                                    )})}
-                            </ul> 
-                        </td>
-                        <td>
-                            <ul>
-                                {Object.entries(order.termekek).map((product,idx) =>{
-                                    return(
-                                        <li key={idx}>{product[1].price}</li>
-                                            )})}
-                            </ul>
-                        </td>
-                        <td>
-                            <ul>
-                                {Object.entries(order.termekek).map((product,idx) =>{
-                                    return(
-                                        <li key={idx}>{product[1].quantity}</li>
-                                            )})}
-                            </ul>
-                        </td>
-                        {
-                        Object.entries(order.termekek).map((product,idx) =>{
-                        sum = sum+product[1].price*product[1].quantity})
-                        }
-                        <td>{sum}</td>
-                    </tr>
-                )})}
-                </tbody>
-        </Table>
-                <div>
-                    <Pager allProducts={orderDatas.filter((order) => (
-                                            userDatas.map((user) => {return user.id}).includes(order.uid))).length}
-                    itemsPerPage={pagerData.itemsPerPage} />
-                </div>
-        </>
+export default function AdminOrdersTable({orders}) {
+    return (
+      <Table responsive striped bordered hover>
+          <thead>
+          <tr>
+              <th>
+                  <ProductNameSorter name="Rendezés Vásárló neve szerint"/>
+              </th>
+          </tr>
+          <tr>
+              <th>Vásárló neve</th>
+              <th>Megrendelés ID</th>
+              <th>Termékek</th>
+              <th>Ár</th>
+              <th>Mennyiség</th>
+              <th>Megrendelés összege</th>
+          </tr>
+          </thead>
+          <tbody>
+          {orders.map((order) => (
+            <tr key={order.id}>
+                <td>{order.fullName}</td>
+                <td> {order.id}</td>
+                <td>
+                    <ul>
+                        {Object.values(order.termekek).map((product) => (
+                            <li key={product.id}>
+                                {product.name}
+                            </li>
+                          )
+                        )}
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        {Object.values(order.termekek).map((product) => (
+                            <li key={product.id}>
+                                {product.price}
+                            </li>
+                          )
+                        )}
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        {Object.values(order.termekek).map((product) => (
+                            <li key={product.id}>
+                                {product.quantity}
+                            </li>
+                          )
+                        )}
+                    </ul>
+                </td>
+                <td>
+                    {Object.values(order.termekek).reduce((sum, product) => (
+                      sum + product.price * product.quantity
+                    ), 0)}
+                </td>
+            </tr>
+          ))}
+          </tbody>
+      </Table>
     )
 }
--- src/pages/AdminOrders.js
+++ src/pages/AdminOrders.js
@@ -1,14 +1,62 @@
 import AdminOrdersTable from "../components/AdminOrders/AdminOrdersTable"
 import Filter from "../components/Filter/Filter"
-import ProductNameSorter from "../components/ProductNameSorter/ProductNameSorter"
+import Pager from "../components/Pager/Pager";
+import { useEffect, useState } from "react";
+import { useSearchParams } from "react-router-dom";
+import pagerService from "../services/pager-service";
+import orderService from "../services/order-service";
+import userService from "../services/user-service";
+import Container from "react-bootstrap/Container";
 
 export default function AdminOrders() {
+  const [orderDatas, setOrderDatas] = useState([]);
+  const [usp] = useSearchParams();
+  const [filteredOrders, setFilteredOrders] = useState([]);
+  const { startIdx, endIdx, itemsPerPage } = pagerService(usp);
+  const name = usp.get("title")
+  const sortBy = usp.get("sortBy")
+  const direction = usp.get("direction")
 
-    return(
-        <div className="container">
-            <Filter />
-            <ProductNameSorter name="Rendezés Vásárló neve szerint"/>
-            <AdminOrdersTable />
-        </div>
-    )
+  useEffect(() => {
+    orderService.getOrders()
+      .then(respOrders => {
+        userService.getUserDatas()
+          .then(userObj => {
+            const orders = Object.values(respOrders);
+            setOrderDatas(
+              orders.map((order) => ({
+                ...order,
+                fullName: `${userObj[order.uid].lastName} ${userObj[order.uid].firstName}`
+              }))
+            );
+          });
+      });
+  }, [])
+
+  useEffect(() => {
+    if (name) {
+      setFilteredOrders(orderDatas.filter((order) => order.fullName.toLowerCase().includes(name.toLowerCase())));
+    } else {
+      setFilteredOrders(orderDatas);
+    }
+  }, [name, orderDatas]);
+
+  useEffect(() => {
+    if (sortBy === 'name') {
+      if (direction === 'asc') setFilteredOrders([...filteredOrders].sort((a, b) => a.fullName.localeCompare(b.fullName)));
+      if (direction === 'desc') setFilteredOrders([...filteredOrders].sort((a, b) => b.fullName.localeCompare(a.fullName)));
+    }
+  }, [direction, sortBy, filteredOrders])
+
+  return (
+    <Container>
+      Megrendelések
+      <Filter/>
+      <AdminOrdersTable orders={filteredOrders.slice(startIdx, endIdx)}/>
+      <Pager
+        allProducts={filteredOrders.length}
+        itemsPerPage={itemsPerPage}
+      />
+    </Container>
+  )
 }

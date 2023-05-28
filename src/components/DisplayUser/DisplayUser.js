53f31ca5721824970dc0043523e57dbcd9de749d
dokahunter <tarmilan@gmail.com>
2023-05-28 22:05:51 +0200

    dropdown menu colors

--- src/components/DisplayUser/DisplayUser.js
+++ src/components/DisplayUser/DisplayUser.js
@@ -4,6 +4,7 @@ import { ToastContext } from "../../services/toastContext";
 import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { CartContext } from "../../contexts/cartContext";
 import { NavDropdown } from "react-bootstrap";
+import "./displayuser.scss";
 
 export default function DisplayUser() {
 
@@ -19,7 +20,7 @@ export default function DisplayUser() {
                         <NavDropdown title={<span>{user.lastName} {user.firstName} {user.isAdmin ? "(admin)" : ""}</span>}>
                             <NavDropdown.Item as={NavLink} to="/profil">Profil</NavDropdown.Item>
                             <NavDropdown.Item as={NavLink} to="/megrendelesek">Rendeléseim</NavDropdown.Item>
-                            <NavDropdown.Item><button onClick={logout}>Kijelentkezés</button></NavDropdown.Item>
+                            <NavDropdown.Item><button className="btn btn-danger" onClick={logout}>Kijelentkezés</button></NavDropdown.Item>
                         </NavDropdown>
                     </>
                     : 

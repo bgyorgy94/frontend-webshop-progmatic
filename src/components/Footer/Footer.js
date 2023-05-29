56135bd9bf2b20100a89738360f84845f41ab741
dokahunter <tarmilan@gmail.com>
2023-05-29 17:30:11 +0200

    fluid footer map

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -32,11 +32,11 @@ export default function Footer() {
                             <li><span className="fw-bold">Telefon:</span> +36-80/9663-828</li>
                         </ul>
                     </Col>
-                    <Col className="d-none d-md-block">
+                    <Col className="d-none d-lg-block">
                         <iframe 
                             src={"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2794.6917622687142!2d17.56629551709081!3d46.810388700000004!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x4769a99a35870f29%3A0x7aac754afbb6b052!2zw4FicmFow6FtaGVneSwgU3ppZ2V0IHUuIDIwLCA4MjU2!5e1!3m2!1shu!2shu!4v1684680730766!5m2!1shu!2shu"} 
-                            width={200} 
-                            height={200} 
+                            className="fluid"
+                            height={"auto"} 
                             allowFullScreen={""} 
                             loading={"lazy" }
                             referrerPolicy={"no-referrer-when-downgrade"} >
